be04cff06c1e01d73c8494726688a503f1b4a74e
Gökhan ALKAN <galkan@outlook.com>
2014-03-19 20:21:02 +0200

    Update open_write_stdio.c

--- others/programming/c/open_write_stdio.c
+++ others/programming/c/open_write_stdio.c
@@ -11,27 +11,19 @@ int main(void)
  
 
         if ( (fd = open(message_name, O_WRONLY|O_CREAT|O_TRUNC,0644)) ==- 1)
-        {
                 fprintf(stderr, "Dosya %s Acilamadi !!!\n", message_name);
-        }
 
         // standart inputdan oku ve fd file descriptoruna yaz
         while( (ret = read(0, buffer, sizeof(buffer))) > 0 )
-        {
                 if ( write(fd, buffer,ret) == -1 )
-                {
                         ;
-                }
-        }
 
         close(fd);
 
         // fd file descriptoru tekrardan okuma modunda ac ve ekrana yazdir
         fd = open(message_name, O_RDONLY);
         while( (ret = read(fd, buffer, sizeof(buffer))) > 0 )
-        {
                 printf("%s\n", buffer);
-        }
 
         close(fd);
 
