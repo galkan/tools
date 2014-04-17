43e048b2f5365d937e95ab1b0559e6789df2fefc
Gökhan ALKAN <galkan@outlook.com>
2014-04-17 10:37:10 +0300

    Update arpspoof.py

--- others/programming/python/arpspoof.py
+++ others/programming/python/arpspoof.py
@@ -14,7 +14,20 @@ except ImportError,e:
         sys.stdout.write("%s\n" %e)
         sys.exit(1)
 
+"""
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
 
+echo "1" > /proc/sys/net/ipv4/ip_forward
+
+"""
 
 	
 class ArpSpoof:
