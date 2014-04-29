adbf125826c32a9039bed9d300d835dd90069582
Gökhan ALKAN <galkan@outlook.com>
2014-04-29 15:09:53 +0300

    Update config_file_parser.py

--- others/programming/python/config_file_parser.py
+++ others/programming/python/config_file_parser.py
@@ -1,43 +1,64 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
 try:
-     import sys
+        import sys
+        import os
+        from ConfigParser import SafeConfigParser
 except ImportError,e:
-     import sys
-     sys.stdout.write("%s\n" %e) 
-     sys.exit(1)
+        import sys
+        sys.stdout.write("%s\n" %e)
+        sys.exit(1)
 
 
-def parse_config_file(config_file)
-     if not os.path.isfile(conf_file):
-          error_message = "%s Dosyasi Sistemde Bulunamadi !!!"% (conf_file)
-          print >> sys.stderr, "Hata: %s"% error_message
-          sys.exit(2)
+def parse_config_file(config_file):
+        """
 
-     try:
-         parser = SafeConfigParser()
-         parser.read(conf_file)
-         for section_name in parser.sections():
-             if section_name == "postgresql":
-                  for name, value in parser.items(section_name):
-                       if name == "host":
-                          host = value
-                       elif name == "password":
-                          password = value
-                       elif name == "user":
-                          user = value
-                       elif name == "database":
-                          database = value
-  
+        """
 
+        if not os.path.isfile(config_file):
+                error_message = "%s Dosyasi Sistemde Bulunamadi !!!"% (config_file)
+                print >> sys.stderr, "Hata: %s"% error_message
+                sys.exit(1)
 
-#[postgresql]
-#host = host
-#database = database
-#password = password
-#user = user
+        result = {}
+        result_line = ("host","password","user", "database")
+
+        try:
+                parser = SafeConfigParser()
+                parser.read(conf_file)
+                for section_name in parser.sections():
+                        if section_name == "postgresql":
+                                for name, value in parser.items(section_name):
+                                        if name == "host":
+                                                result["host"] = value
+                                        elif name == "password":
+                                                result["password"] = value
+                                        elif name == "user":
+                                                result["user"] = value
+                                        elif name == "database":
+                                                result["database"] = value
+                for line in result_line:
+                        if not line in result.keys():
+                                result[line] = "-"
+
+                return result
+except Exception, err:
+                print >> sys.stderr, err
+                sys.exit(1)
+
 
 if __name__ == "__main__":
-     db_conf_file = "/etc/db.conf"
-     
+        try:
+                conf_file = sys.argv[1]
+        except:
+                print >> sys.stderr, "Komut Satiri Argumani Eksik !!!"
+                sys.exit(1)
+
+        for val in parse_config_file(conf_file).keys():
+                print val,parse_config_file(conf_file)[val]
+
+#[postgresql]
+#host = 127.0.0.1
+#database = user_pass
+#passsword = Test12345
+#user = deneme
