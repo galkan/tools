0cd3764bf686ad4cce7cb92004ae9304c1ebf193
Gökhan ALKAN <galkan@outlook.com>
2014-05-22 09:38:47 +0300

    Update run_command_via_ssh.py

--- others/poc/run_command_via_ssh.py
+++ others/poc/run_command_via_ssh.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python
 
-__VERSION__ = '0.1'
-__AUTHOR__ = 'Galkan'
-__DATE__ = '2014'
-
 """
-In order to install paramiko, follow steps below...
+In order to install paramiko, follow steps below... 
+# apt-get install pip
+# wget https://pypi.python.org/packages/source/p/pip/pip-1.5.6.tar.gz#md5=01026f87978932060cc86c1dc527903e
+# tar -zxfv pip-1.5.6.tar.gz
+# cd pip-1.5.6
+# python setup.py install
+# pip install ecsda
 # wget https://github.com/paramiko/paramiko/archive/master.zip
 # unzip master.zip
 # python setup.py install
-# apt-get install pip
-# pip install ecsda
 """
 
-#echo -e "Test\nTest" | passwd root
-
 try:
-        import paramiko
-        import argparse
-        import sys
-        import os
-        import threading
-        import Queue
-        import time
+	import paramiko 
+	import argparse
+	import sys
+	import os
+	import threading
+	import Queue
+	import time
 except ImportError,e:
         import sys
         sys.stdout.write("%s\n" %e)
@@ -44,8 +42,9 @@ class SshClient:
 
     def execute(self, command):
         stdin, stdout, stderr = self.client.exec_command(command)
-        return {'out': stdout.readlines(), 'err': stderr.readlines(), 'retval': stdout.channel.recv_exit_status()}
+	return {'out': stdout.readlines(), 'err': stderr.readlines(), 'retval': stdout.channel.recv_exit_status()}
 
+	
     def close(self):
         if self.client is not None:
             self.client.close()
@@ -53,61 +52,69 @@ class SshClient:
 
 
 class Main:
-        def __init__(self):
+	def __init__(self):
 
-                description = "Kullanim Parametreleri"
-                usage = "Usage: use --help for futher information"
-                parser = argparse.ArgumentParser(description = description, usage = usage)
-                parser.add_argument('-c','--config', dest = 'config', help = 'Configuration File', required = True)
-                parser.add_argument('-t','--thread', dest = 'thread', help = 'Thread Number', required = False)
-                self.args = parser.parse_args()
+		description = "Kullanim Parametreleri"
+        	usage = "Usage: use --help for futher information"
+        	parser = argparse.ArgumentParser(description = description, usage = usage)
+        	parser.add_argument('-c','--config', dest = 'config', help = 'Configuration File', required = True)
+        	parser.add_argument('-t','--thread', dest = 'thread', help = 'Thread Number', required = False)
+        	self.args = parser.parse_args()
 
-                if not os.path.exists(self.args.config):
-                        print >> sys.stderr, "%s Doesn't Exists"% (self.args.config)
-                        sys.exit(1)
+        	if not os.path.exists(self.args.config):
+                	print >> sys.stderr, "%s Doesn't Exists"% (self.args.config)
+                	sys.exit(1)
 
-                self.queue_full = True
+		self.queue_full = True
 
 
-        def execute_command(self, queue):
-
-                while self.queue_full:
+	def execute_command(self, queue):
+	
+		while self.queue_full:
                         try:
                                 cred_line = queue.get(False)
-                                ip_addr = cred_line.split(":")[0]
-                                user = cred_line.split(":")[1]
-                                passwd = cred_line.split(":")[2]
-                                command = cred_line.split(":")[3].split(";")
+				ip_addr = cred_line.split(":")[0]
+				user = cred_line.split(":")[1]
+				passwd = cred_line.split(":")[2]
+				command = cred_line.split(":")[3].split(";")
 
-                                try:
-                                        client = SshClient(host = ip_addr, port = 22, username = user, password = passwd)
-                                except Exception, err:
-                                        print >> sys.stderr, "%s : %s"% (ip_addr, str(err))
-                                        sys.exit(1)
+				try:
+					client = SshClient(host = ip_addr, port = 22, username = user, password = passwd)
+				except Exception, err:
+					print >> sys.stderr, "%s : %s"% (ip_addr, str(err))
+					sys.exit(1)
 
-                                try:
-                                        for cmd in command:
-                                                ret = client.execute(cmd)
-                                                print "IP: %s - Command: %s - %d"% (ip_addr, cmd, ret["retval"])
-                                finally:
-                                        client.close()
-
+				try:
+					for cmd in command:
+						ret = client.execute(cmd)
+						if ret["retval"] == 0:
+							print "SUCCCESS: %s - %s : %d\n"% (ip_addr, cmd[:-1], ret["retval"])
+							for line in ret["out"]:
+								print line[:-1]
+						else:
+							print "FAIL: %s - %s : %d\n"% (ip_addr, cmd[:-1], ret["retval"])
+				finally:
+					client.close()		
+			
                         except Queue.Empty:
                                 self.queue_full = False
+			
 
+	def run(self):
 
-        def run(self):
+		q = Queue.Queue()
+		for line in open(self.args.config,"r"):
+			if line[0] == "#":
+				continue
+			else:
+				q.put(line)
 
-                q = Queue.Queue()
-                for line in open(self.args.config,"r"):
-                        q.put(line)
+		if self.args.thread is None:
+			self.args.thread = 1
 
-                if not self.args.thread:
-                        self.args.thread = 5                
-
-                for i in range(int(self.args.thread)):
-                        t = threading.Thread(target = self.execute_command, args = (q,))
-                        t.start()
+		for i in range(int(self.args.thread)):
+			t = threading.Thread(target = self.execute_command, args = (q,))
+			t.start()
 
 
 ##
@@ -115,6 +122,6 @@ class Main:
 ##
 
 if __name__ == "__main__":
-
-        main = Main()
-        main.run()
+	
+	main = Main()
+	main.run()
