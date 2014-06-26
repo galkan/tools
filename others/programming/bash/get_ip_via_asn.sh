650e1bacb02bd515d893138b9cbc246735825289
Gökhan ALKAN <galkan@outlook.com>
2014-06-26 16:06:53 +0300

    Update get_ip_via_asn.sh

--- others/programming/bash/get_ip_via_asn.sh
+++ others/programming/bash/get_ip_via_asn.sh
@@ -12,32 +12,32 @@ ip_list=0
 
 if [ "$#" -eq 2 ] && [ "$1" == "-d" ]
 then
-        domain="$2"
+	domain="$2"
 elif [ "$#" -eq 4 ] && [ "$3" == "-i" ] && [ $4 -eq 0 -o $4 -eq 1 ]
 then
-        domain="$2"
-        ip_list="$4"
+	domain="$2"
+	ip_list="$4"
 else
-        echo "Usage: $0 -d <domain> -i <1|0>"
-        exit 1
+	echo "Usage: $0 -d <domain> -i <1|0>"
+	exit 1
 fi
 
 for cmd_file in $host_path $whois_path
 do
-        if [ ! -f $cmd_file ]
-        then
-                echo "File $cmd_file Doesn't Exists On The System."
-                exit 1
-        fi
+	if [ ! -f $cmd_file ]
+	then
+		echo "File $cmd_file Doesn't Exists On The System."
+		exit 1
+	fi
 done
 
-if [ $ip_list -eq 1 ]
+if [ $ip_list -eq 1 ] 
 then
-        if [ ! -f $prips_path ]
-        then
-                echo "File $prips_path Doesn't Exists On The System."
-                exit 1
-        fi
+	if [ ! -f $prips_path ]
+	then
+		echo "File $prips_path Doesn't Exists On The System."
+		exit 1
+	fi	
 fi
 
 ip_addr_file="`mktemp /tmp/$USER.XXXXXX`"
@@ -46,49 +46,23 @@ whois_domain="asn.shadowserver.org"
 
 function getip()
 {
-        domain="$1"
+	domain="$1"
 
-        host $domain | while read -r line
-        do
-                echo "$line" | grep -Eq "has address [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
-                if [ $? -eq 0 ]
-                then
-                        echo $line | cut -d " " -f4 >> $ip_addr_file
-                fi
-done
+	host $domain | while read -r line
+	do
+        	echo "$line" | grep -Eq "has address [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+        	if [ $? -eq 0 ]
+        	then
+                	echo $line | cut -d " " -f4 >> $ip_addr_file
+       	 	fi
 
-if [ $ip_list -eq 1 ]
-then
-        if [ ! -f $prips_path ]
-        then
-                echo "File $prips_path Doesn't Exists On The System."
-                exit 1
-        fi
-fi
-
-ip_addr_file="`mktemp /tmp/$USER.XXXXXX`"
-prefix_file="`mktemp /tmp/$USER.XXXXXX`"
-whois_domain="asn.shadowserver.org"
-
-function getip()
-{
-        domain="$1"
-
-        host $domain | while read -r line
-        do
-                echo "$line" | grep -Eq "has address [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
-                if [ $? -eq 0 ]
-                then
-                        echo $line | cut -d " " -f4 >> $ip_addr_file
-                fi
-
-                echo "$line" | grep -Eq "is handled by [0-9]+ "
-                if [ $? -eq 0 ]
-                then
-                        new_domain="`echo $line | cut -d " " -f7`"
-                        getip "$new_domain"
-                fi
-        done
+        	echo "$line" | grep -Eq "is handled by [0-9]+ "
+        	if [ $? -eq 0 ]
+        	then
+                	new_domain="`echo $line | cut -d " " -f7`"
+			getip "$new_domain"
+        	fi
+	done
 
 }
 
@@ -96,20 +70,20 @@ getip "$domain"
 
 sort -n $ip_addr_file | uniq | while read -r ip
 do
-        whois -h $whois_domain "origin $ip" | cut -d "|" -f1 | tr -d ' ' >> $prefix_file
+	whois -h $whois_domain "origin $ip" | cut -d "|" -f1 | tr -d ' ' >> $prefix_file 	
 done
 
 sort -n $prefix_file | uniq | while read -r prefix
 do
-        if [ $ip_list -eq 1 ]
-        then
-                whois -h $whois_domain "prefix $prefix" | while read -r cidr
-                do
-                        $prips_path $cidr
-                done
-        else
-                whois -h $whois_domain "prefix $prefix"
-        fi
+	if [ $ip_list -eq 1 ]
+	then
+		whois -h $whois_domain "prefix $prefix" | while read -r cidr
+		do
+			$prips_path $cidr
+		done
+	else
+		whois -h $whois_domain "prefix $prefix" 
+	fi
 done
 
 rm -rf $ip_addr_file $prefix_file
