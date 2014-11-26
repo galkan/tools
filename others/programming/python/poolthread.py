683ca0219b69a047793b63e441ffa323f2efb101
Gökhan ALKAN <cigalkan@gmail.com>
2014-11-26 12:06:32 +0200

    Create poolthread.py

---
+++ others/programming/python/poolthread.py
@@ -0,0 +1,89 @@
+try:
+        import sys
+        import inspect
+        from Queue import Queue
+        from threading import Thread
+except ImportError, err:
+        import sys
+        sys.stderr.write("%s : %s-%s\n"% (err, __file__, inspect.currentframe().f_lineno))
+        sys.exit(1)
+
+
+
+class Worker(Thread):
+
+    def __init__(self, tasks):
+
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+
+    def run(self):
+        """
+                Thread basladiginda calisacak kod. self.start() dendiginde calisacak kod parcai
+        """
+
+        while True:
+                func, args, kargs = self.tasks.get(True, None)
+
+                if not func:
+                        break
+
+                try:
+                        func(*args, **kargs)
+                except Exception, err:
+                        pass
+
+                self.tasks.task_done()
+
+
+
+class ThreadPool:
+
+    def __init__(self, num_threads):
+
+        self.threads = []
+
+        self.num_threads = num_threads
+        self.tasks = Queue(self.num_threads)
+
+        for _ in range(self.num_threads): 
+                worker = Worker(self.tasks)
+                self.threads.append(worker)
+
+
+    def add_task(self, func, *args, **kargs):
+
+        self.tasks.put((func, args, kargs))
+
+
+
+    def wait_completion(self):
+
+        self.tasks.join()
+
+        for _ in range(self.num_threads):
+                self.add_task(None, None, None)
+
+        for t in self.threads:
+                t.join()
+
+
+def foo(line):
+   print line
+
+##
+### Main
+##
+
+
+
+if __name__ == "__main__":
+  pool = ThreadPool(10)
+  
+  for line in open("/etc/passwd", "r"):
+    pool.add_task(foo, line)
+   
+  pool.wait_completion() 
